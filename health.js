9cf429c7b80fde0247d1021621dcc6e90eaac153
ry069353-gif <ry069353@gmail.com>
2026-08-13 16:42:29 +0530

    Delete health.js

--- health.js
+++
@@ -1,6 +0,0 @@
-import { applyCors } from '../lib/auth.js';
-
-export default function handler(req, res) {
-  if (applyCors(req, res)) return;
-  res.status(200).json({ status: 'ok', service: 'English Vocab Backend', version: '1.0.0' });
-}
